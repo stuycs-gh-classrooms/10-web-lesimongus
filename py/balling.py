bc8c2a9e3bdad758476be48899e8f3ff0b1cf27d
lesimongus <sliu60@stuy.edu>
2024-06-03 15:07:52 -0400

    added a bit more code

--- py/balling.py
+++ py/balling.py
@@ -5,3 +5,23 @@ import cgitb
 cgitb.enable()
 
 import cgi
+
+from random import randint
+
+header = """
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+<title>Hello</title>
+</head>
+"""
+
+footer = """
+</body>
+</html>
+"""
+
+roll = randint(1, 20)
+print(roll)
