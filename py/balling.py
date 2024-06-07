1538a1ae4ddaf690bd6139bbdae5b94a15b5f36f
lesimongus <sliu60@stuy.edu>
2024-06-07 15:27:51 -0400

    tried to add a form to the next page

--- py/balling.py
+++ py/balling.py
@@ -26,8 +26,26 @@ name = 'egg'
 if ('name' in data):
     name = data['name'].value
 
+def choices(options, subject, question):
+    form = """
+<form action="balling.py" method="GET">
+"""
+    form += question
+    buttons = ''
+    for stuff in options:
+        choice = '<div>'
+        choice += 'input type="radio" name="' + subject + '"value="' + stuff + '">'
+        choice+= stuff + '</div>'
+        buttons+= choice
+    form+= buttons
+    form+= '<input type="submit" value="Tell">'
+    return form
+
 html= HTML_HEADER
 html+= '<h1>Awaken, ' + name + '</h1>'
+html+= choices(['cool beans', 'kind fellow', 'I refuse to answer!'], 'description', 'What are you?')
 html+= '<br><a href="TitlePending.html">Try Again</a>'
 html+= HTML_FOOTER
 print(html)
+
+
